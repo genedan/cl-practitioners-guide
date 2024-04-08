cf921cd3060dccad9fc3f11e3c194aea5dd1c34d
Gene Dan <genedan@gmail.com>
2024-04-08 17:12:39 -0500

    DOCS: Add Sphinx configuration file.

---
+++ conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Practitioner's Guide to Reserving Using Open-Source Tools"
+copyright = '2024, John Bogaardt, Gene Dan, Kenneth Hsu'
+author = 'John Bogaardt, Gene Dan, Kenneth Hsu'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import chainladder as cl
+
+extensions = [
+    "IPython.sphinxext.ipython_directive",
+    "nbsphinx"
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv', 'tmp']
+# latex_additional_files = ['preamble.sty']
+# latex_elements = {
+#     'preamble': r'''\usepackage{preamble}'''
+# }
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
